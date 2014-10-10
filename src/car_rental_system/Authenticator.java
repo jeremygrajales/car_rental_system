2ac0aea8cc398bc5ed9ea4b872c3ac3b05c198fa
Jeremy L. Grajales <jeremygrajales@gmail.com>
2014-10-10 02:45:34 -0500

    Added correct authenticate method

--- src/car_rental_system/Authenticator.java
+++ src/car_rental_system/Authenticator.java
@@ -6,9 +6,6 @@ public class Authenticator {
 
 	public static boolean authenticate(String email, String password) {
 
-		// Generate hash
-		String hash = hash(password);
-		
 		// Clean input
 		email = Helper.cleanString(email);
 		password = Helper.cleanString(password);
@@ -17,7 +14,7 @@ public class Authenticator {
 		Connection conn = Database.connect();
 
 		// Make SQL query
-		String query = "SELECT COUNT(*) AS rowCount FROM `customer` WHERE `cust_email`='"+email+"' AND `hash`='"+hash+"';";
+		String query = "SELECT * FROM `customer` WHERE `cust_email`='"+email+"';";
 
 		Statement stmt = null;
 		try {
@@ -28,8 +25,16 @@ public class Authenticator {
 			ResultSet result = stmt.executeQuery(query);
 			result.next();
 			
+			// Get salt from hash
+			String[] hashTuple = result.getString("hash").split(":");
+			String originalHash = hashTuple[0];
+			String salt = hashTuple[1];
+			
+			// Recreate hash
+			String newHash = hash(password, salt);
+			
 			// Return TRUE if we found a user with given credentials, else return FALSE
-			return result.getInt("rowCount") > 0;
+			return newHash.equals(originalHash);
 			
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
@@ -39,10 +44,16 @@ public class Authenticator {
 		return false;
 
 	}
-	
 
-	public static String hash(String value) {
-		return value; // TODO: implement hashing with salt
+	public static String hash(String password, String salt) {
+		return ""; 
+		// TODO: implement hashing with salt
+		// return hash and salt in String
+		// use ':' to delimit hash and salt 
+		// eg. hash:salt
+		// salt = mcrypt(random integer)
+		// hash = SHA256(password+salt)
+		// return = hash:salt
 	}
 	
 	
