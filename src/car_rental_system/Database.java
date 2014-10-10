76f67656c611716e0e5015f6467897ee5b3d8e17
Jeremy L. Grajales <jeremygrajales@gmail.com>
2014-10-10 03:49:36 -0500

    Forgot to load jdbc driver with Class.forName()

--- src/car_rental_system/Database.java
+++ src/car_rental_system/Database.java
@@ -1,5 +1,6 @@
 package car_rental_system;
 
+import com.mysql.jdbc.Driver;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -8,15 +9,24 @@ import java.util.Properties;
 public class Database {
 	
 	public static Connection connect() {
+		
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+		} catch (ClassNotFoundException e) {
+		    throw new RuntimeException("JDBC Driver not loaded. Cannot find the driver in the classpath!", e);
+		}
+
 		 Connection conn = null;
 		 Properties props = new Properties();
 		 props.put("user", "root"); // TODO: implement configuration file
-		 props.put("password", "password"); // TODO: implement configuration file 
+		 props.put("password", "password"); // TODO: implement configuration file
+		 
 		 try {
-		 	conn = DriverManager.getConnection("jdbc:mysql://vps.neiucss.org:3306/car_rental_system", props);
+		 	conn = DriverManager.getConnection("jdbc:mysql://vps.neiucss.org/car_rental_system", props);
 		 }
 		 catch(SQLException e) {
 			 // TODO: implement
+			 System.out.println(e.getMessage());
 		 }
 		 return conn;
 	}
