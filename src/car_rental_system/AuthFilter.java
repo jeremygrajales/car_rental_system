29a7846f9b780eed722c5fb1dae89def0191a455
Jeremy L. Grajales <jeremygrajales@gmail.com>
2014-10-17 06:58:27 -0500

    Added AuthFilter.

---
+++ src/car_rental_system/AuthFilter.java
@@ -0,0 +1,53 @@
+package car_rental_system;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.*;
+
+/**
+ * Servlet Filter implementation class AuthFilter
+ */
+@WebFilter("/AuthFilter")
+public class AuthFilter implements Filter {
+
+    /**
+     * Default constructor. 
+     */
+    public AuthFilter() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see Filter#destroy()
+	 */
+	public void destroy() {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
+	 */
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		
+	
+		if(((HttpServletRequest)request).getSession().getAttribute("currentUser") == null) {
+			((HttpServletResponse)response).sendRedirect("login.jsp");	
+		}
+		chain.doFilter(request, response);
+	}
+
+	/**
+	 * @see Filter#init(FilterConfig)
+	 */
+	public void init(FilterConfig fConfig) throws ServletException {
+		// TODO Auto-generated method stub
+	}
+
+}
