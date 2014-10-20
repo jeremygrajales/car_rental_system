e84b6cd6bf91bc69b5d6a0693a05d85ef0752620
Noman Rauf <nrauf@neiu.edu>
2014-10-20 15:11:22 -0500

    added last of form validation. used regular expression in java.util

--- src/car_rental_system/FormValidator.java
+++ src/car_rental_system/FormValidator.java
@@ -1,19 +1,19 @@
 package car_rental_system;
 
+import java.util.*;
 import java.util.LinkedHashMap;
 
 public class FormValidator {
 	
-	// Leap year validation
+	// Leap year validation // return true if valid
 	public static boolean validateDate(int year, int month, int day) {
 		
 		//check to see if february 29 is valid for given year
-		if(month == 2 and day == 29){
-			if ((year % 400 == 0) || ((year % 4 == 0) && (year % 100 != 0))) {
-				return true;
-		}
-		
-		return false;
+		if(month == 2 && day == 29){
+			if ((year % 400 != 0) || ((year % 4 != 0) && (year % 100 == 0))) {
+				return false;
+		} 
+		return true;
 	}
 	
 	// Returns false if input is greater than length
@@ -42,8 +42,21 @@ public class FormValidator {
 	
 	// Returns false if not a strong enough password
 	public static boolean validatePasswordStrength(String input) {
-		return true;
+		
+		//checks to see if there is at least 1 capital letter, 1 lower case and 1 digit. minimum = 8 maximum = 20
+		Pattern pattern = Pattern.compile("((?=.*[a-z])(?=.*\\d)(?=.*[A-Z]).{8,20})");
+		
+		//match given password to specified pattern
+		Matcher matcher = pattern.matcher(input);
+		
+		// returns true if pattern is matched
+		if(matcher.matches() == true)
+			return true;
+					
+		return false;
 	}
+		
+	
 	
 	public static boolean validatePasswordMatch(String pwd1, String pwd2) {
 		
